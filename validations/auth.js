c3c91d3d35bdd5a807d952b54fec61b3920988ab
voin12K <vladleurda@gmail.com>
2024-05-23 12:12:41 +0200

    fix

--- validations/auth.js
+++ validations/auth.js
@@ -1,8 +1,8 @@
 const { body } = require('express-validator');
 
 exports.registerValidation = [
-    body('email').isEmail(),
-    body('password').isLength({ min: 8 }),
-    body('fullName').isLength({ min: 3 }), 
-    body('avatarUrl').optional().isURL(),
+    body('email', 'Invalid email format').isEmail(),
+    body('password', 'The password must contain at least 8 characters').isLength({ min: 8 }),
+    body('fullName', 'Minimum name length 3 characters').isLength({ min: 3 }), 
+    body('avatarUrl', 'Invalid link format').optional().isURL(),
 ];
