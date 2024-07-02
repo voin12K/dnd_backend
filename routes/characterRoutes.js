c42ebae75b1348ea64977870e195388085854b0a
voin12k <vladleurda02@gmail.com>
2024-07-02 15:38:24 +0200

    good

--- routes/characterRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { createCharacter } from '../controllers/characterController.js';
-
-const router = express.Router();
-
-router.post('/', createCharacter);
-
-export default router;
--- routes/roomRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createRoom, joinRoom } from '../controllers/roomController.js';
-
-const router = express.Router();
-
-router.post('/create', createRoom);
-router.post('/join', joinRoom);
-
-export default router;
